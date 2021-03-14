5818d86a37da4641c094a360f7ba8e70c0a33f0a
marceloalvarezpm <77630352+marceloalvarezpm@users.noreply.github.com>
2021-03-13 22:55:52 -0500

    Update index.js
    Changed Header

--- src/pages/index.js
+++ src/pages/index.js
@@ -51,7 +51,7 @@ export default function Home({data}) {
       <HeaderLogo />
       <Layout>
         <Hero>
-          <HeadingXL>Lewis Gatsby Starter Blog</HeadingXL>
+          <HeadingXL>Marcelo Alvarez, Product Owner</HeadingXL>
           <TextHome>
             This is a custom Gatsby starter template to start a new blog or
             personal website.
