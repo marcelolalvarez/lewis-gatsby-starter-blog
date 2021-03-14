e4060150b2a72db33c185cdcb0fcd7a2ea111613
marceloalvarezpm <77630352+marceloalvarezpm@users.noreply.github.com>
2021-03-14 08:49:33 -0400

    Update about.js
    about

--- src/pages/about.js
+++ src/pages/about.js
@@ -20,14 +20,6 @@ export default function About({data}) {
         <Image fluid={data.RandomPhoto.childImageSharp.fluid} />
         <TextBody>
         A professional with strong experience in high functioning product development and project management environments. Highly adaptable with recognized talents working with E-Commerce solutions such as Magento as well as being proficient in aligning software requirements with innovative solutions to complex IT challenges. Continually on the forefront of developing trends integrating current and emerging cutting-edge technologies and peripheral processes to deliver impactful results.
-          <br />
-          <br />
-          Bringing so sociable felicity supplied mr. September suspicion far him
-          two acuteness perfectly. Covered as an examine so regular of. Ye
-          astonished friendship remarkably no. Window admire matter praise you
-          bed whence. Delivered ye sportsmen zealously arranging frankness
-          estimable as. Nay any article enabled musical shyness yet sixteen yet
-          blushes. Entire its the did figure wonder off.
         </TextBody>
         <Button href="mailto:marcelo.alvarez@me.com">Get in touch</Button>
       </Layout>
