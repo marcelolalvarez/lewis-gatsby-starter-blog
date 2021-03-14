9d2aef72e4ae9945917ddb782b3a61287840942b
marceloalvarezpm <77630352+marceloalvarezpm@users.noreply.github.com>
2021-03-14 14:43:03 -0400

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -23,9 +23,8 @@ export default function About({data}) {
         A professional with strong experience in high functioning product development and project management environments. Highly adaptable with recognized talents working with E-Commerce solutions such as Magento as well as being proficient in aligning software requirements with innovative solutions to complex IT challenges. Continually on the forefront of developing trends integrating current and emerging cutting-edge technologies and peripheral processes to deliver impactful results.<br />
         </TextBody>
 
-        <HeadingL>EXPERTISE</HeadingL>
+        <HeadingL>Professional Competencies</HeadingL>
         <TextBody>
-        Professional Completencies<br /><br />
         Project Management<br />
         Product Management<br />
         Agile<br />
