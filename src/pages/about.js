9fd0d2ff09da94f26382b4dc00c72fb0cee56b85
marceloalvarezpm <77630352+marceloalvarezpm@users.noreply.github.com>
2021-03-14 20:19:10 -0400

    new page
    New Page

--- src/pages/about.js
+++ src/pages/about.js
@@ -22,30 +22,7 @@ export default function About({data}) {
         <TextBody>
         A professional with strong experience in high functioning product development and project management environments. Highly adaptable with recognized talents working with E-Commerce solutions such as Magento as well as being proficient in aligning software requirements with innovative solutions to complex IT challenges. Continually on the forefront of developing trends integrating current and emerging cutting-edge technologies and peripheral processes to deliver impactful results.<br />
         </TextBody>
-
-        <HeadingL>Professional Competencies</HeadingL>
-        <TextBody>
-        Project Management<br />
-        Product Management<br />
-        Agile<br />
-        Waterfall<br />
-        SDLC<br />
-        Strategic Partnerships<br />
-        Usability Testing<br />
-        Web/Application Design<br />
-        SEO/SEM<br />
-        Agency Management<br />
-        Optimization<br />
-        Exec Communication<br />
-        Analytics<br />
-        Ecommerce Strategy<br />
-        Cloud<br />
-        Business Requirements<br />
-        Team Buildup<br />
-        Outsourcing Specialist<br />
-        Budget Proposals<br />
-        KPI Measurement<br /><br />
-       </TextBody>
+       
         <Button href="mailto:marcelo.alvarez@me.com">Get in touch</Button>
       </Layout>
     </>
---
+++ src/pages/expertise.js
@@ -0,0 +1,69 @@
+import React from "react"
+import {graphql} from "gatsby"
+import {
+  Button,
+  HeaderBack,
+  HeadingXL,
+  Image,
+  Layout,
+  SEO,
+  TextBody,
+} from "../components"
+
+export default function expertise({data}) {
+  return (
+    <>
+      <SEO title="Marcelo Alvarez, Full Stack E-commerce Project Manager" />
+      <HeaderBack />
+      <Layout>
+        <HeadingXL>Experience</HeadingXL>
+        <Image fluid={data.RandomPhoto.childImageSharp.fluid} />
+        <TextBody>
+        Professional Competencies<br /> <br />
+        Project Management<br />
+        Product Management<br />
+        Agile<br />
+        Waterfall<br />
+        SDLC<br />
+        Strategic Partnerships<br />
+        Usability Testing<br />
+        Web/Application Design<br />
+        SEO/SEM<br />
+        Agency Management<br />
+        Optimization<br />
+        Exec Communication<br />
+        Analytics<br />
+        Ecommerce Strategy<br />
+        Cloud<br />
+        Business Requirements<br />
+        Team Buildup<br />
+        Outsourcing Specialist<br />
+        Budget Proposals<br />
+        KPI Measurement<br /><br />
+
+        Platform Expertise<br /><br />
+        Project Management - Jira, Trello, BaseCamp, Smartsheet, Microsoft Project, Freedcamp.
+Analytics - Google Analytics, WebTrends
+Ecommerce - Magento, Woocommerce
+CMS - Word Press, Drupal
+System Diagramming - Lucidchart, Microsoft Visio, Balsamiq
+Design - Photoshop
+Other - MS Office 365, OneNote, Sharepoint 365, Confluence, Zendesk, Kayako.
+        </TextBody>
+        <Button href="mailto:marcelo.alvarez@me.com">Get in touch</Button>
+      </Layout>
+    </>
+  )
+}
+
+export const data = graphql`
+  query {
+    RandomPhoto: file(relativePath: {eq: "assets/images/fullstack.png"}) {
+      childImageSharp {
+        fluid(maxWidth: 1400) {
+          ...GatsbyImageSharpFluid_withWebp
+        }
+      }
+    }
+  }
+`
