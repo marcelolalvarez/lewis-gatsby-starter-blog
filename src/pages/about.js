47ec703fa42b7209399406eb4d0c02a034a17bc8
marceloalvarezpm <77630352+marceloalvarezpm@users.noreply.github.com>
2021-03-15 22:25:04 -0400

    Update about.js
    added 2 links

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,4 +1,5 @@
 import React from "react"
+import {Link} from "gatsby"
 import {graphql} from "gatsby"
 import {
   Button,
@@ -20,7 +21,16 @@ export default function About({data}) {
         <HeadingXL>Full Stack E-commerce Project Manager</HeadingXL>
         <Image fluid={data.RandomPhoto.childImageSharp.fluid} />
         <TextBody>
-        A professional with strong experience in high functioning product development and project management environments. Highly adaptable with recognized talents working with E-Commerce solutions such as Magento as well as being proficient in aligning software requirements with innovative solutions to complex IT challenges. Continually on the forefront of developing trends integrating current and emerging cutting-edge technologies and peripheral processes to deliver impactful results.<br />
+        A professional with strong experience in high functioning product development and project management environments. Highly adaptable with recognized talents working with E-Commerce solutions such as Magento as well as being proficient in aligning software requirements with innovative solutions to complex IT challenges. Continually on the forefront of developing trends integrating current and emerging cutting-edge technologies and peripheral processes to deliver impactful results.<br /><br />
+        <Link to="/experience">
+        <p>Experience</p>
+      </Link>
+
+      <Link to="/expertise">
+        <p>Expertise</p>
+      </Link>
+
+        
         </TextBody>
        
         <Button href="mailto:marcelo.alvarez@me.com">Get in touch</Button>
