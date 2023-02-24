c40c71a5ff84a698eb13e6d3f47737d863db07ba
Marcelo Alvarez <53400194+marcelolalvarez@users.noreply.github.com>
2023-02-23 20:34:19 -0500

    Update about.js
    Updated title

--- src/pages/about.js
+++ src/pages/about.js
@@ -17,7 +17,7 @@ export default function About({data}) {
       <SEO title="Marcelo Alvarez, IT Infrastructure Project Manager" />
       <HeaderLogo />
       <Layout>
-        <HeadingXL>Full Stack E-commerce Project Manager</HeadingXL>
+        <HeadingXL>IT Infrastructure Project Manager</HeadingXL>
         <img src={imageAsset} alt='About' />
         <TextBody>
         A professional with strong experience in high functioning product development and project management environments. Highly adaptable with recognized talents working with E-Commerce solutions such as Magento as well as being proficient in aligning software requirements with innovative solutions to complex IT challenges. Continually on the forefront of developing trends integrating current and emerging cutting-edge technologies and peripheral processes to deliver impactful results.<br /><br />
